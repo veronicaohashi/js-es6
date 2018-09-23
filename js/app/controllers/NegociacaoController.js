18220d89eec1e3edb524cd0bf85ebcd80851e5b9
veronicaohashi <veronica.ohashi@outlook.com>
2018-09-23 17:18:00 -0300

    Adicionado solução utilizando proxy

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -19,14 +19,44 @@ class NegociacaoController {
     //   // Model - instâcia de lista negociações que vai ser passada para a função quando for chamada
     //   this._negociacoesView.update(model);
     // });
+    let self = this;
+    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+      // Método (armadilha) chamado toda vez que for realizado uma operação de leitura do objeto
+      // target - referencia ao objeto original que está sendo encapsulado pelo proxy
+      // prop - propriedade que está sendo acessada
+      // receiver - referencia para o proxy
+      get(target, prop, receiver){
+        if (['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) === typeof(Function)){
+          // Deve ser function para ter o this dinâmico.
+          // Não pode ser arrow function que possui um escopo léxico.
+          // Vou substituir esse método proxy por outro
+          return function(){
+            console.log(`interceptando ${prop}`);
+            // A função passa a receber os parâmetros dela
+            // Argument - variável implícita que me da acesso a todos os parametros da função quando é chamada
+            Reflect.apply(target[prop], target, arguments);
+            self._negociacoesView.update(target);
+          }
+        }
+        // console.log(`a propriedade "${prop}" foi interceptada`);
+        // Informo o valor retornado após ter interceptado a propriedade de leitura
+        return Reflect.get(target, prop, receiver);
+      },
+
+      // set: function(target, prop, value, receiver){
+      //   // target[prop] - passo o nome da propriedade que quero acessar desse objeto
+      //   console.log(`valor anterior ${target[prop]} e valor atual ${value}`);
+      //   return Reflect.set(target, prop, value, receiver);
+      // }
+    });
 
     // O escopo do this de uma arrow function é lexico, e não dinâmico. Sendo assim, ele não muda de 
     // acordo com o contexto
-    this._listaNegociacoes = new ListaNegociacoes(model =>      
-      // Atualiza a view para exibir a tabela mesmo com dados vazios
-      // Model - instâcia de lista negociações que vai ser passada para a função quando for chamada
-      this._negociacoesView.update(model)
-    );
+    // this._listaNegociacoes = new ListaNegociacoes(model =>      
+    //   // Atualiza a view para exibir a tabela mesmo com dados vazios
+    //   // Model - instâcia de lista negociações que vai ser passada para a função quando for chamada
+    //   this._negociacoesView.update(model)
+    // );
 
     // Passa o elemento do DOM para o constructor
     this._negociacoesView  = new NegociacoesView($('#negociacoesView'));
